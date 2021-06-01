61a9a223a6f210b1c7842b467c3fa560350137a7
Jason <bilafish@gmail.com>
2021-06-01 18:38:05 +0800

    chore: setup firebase

---
+++ helpers/firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase/app'
+import 'firebase/auth';
+import 'firebase/firestore';
+import 'firebase/storage';
+
+const firebaseConfig = {
+    // your config
+};
+  
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig)
+}
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+export const storage = firebase.storage();
