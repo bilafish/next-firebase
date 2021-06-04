10417e66ed9b15147b672de3736fd4da26dd4e4a
Jason <bilafish@gmail.com>
2021-06-04 21:54:10 +0800

    feat: add google auth sign in

--- helpers/firebase.js
+++ helpers/firebase.js
@@ -1,23 +1,23 @@
-import firebase from 'firebase/app'
-import 'firebase/auth';
-import 'firebase/firestore';
-import 'firebase/storage';
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+import "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAMojsNGnS5RZAUH-hpgYfmr3SxVg6-TdI",
-    authDomain: "nextfire-f878b.firebaseapp.com",
-    projectId: "nextfire-f878b",
-    storageBucket: "nextfire-f878b.appspot.com",
-    messagingSenderId: "531960495717",
-    appId: "1:531960495717:web:397df87a299bd0396e108c",
-    measurementId: "G-WKFLHQN0EJ"
-  };
+  apiKey: "AIzaSyAMojsNGnS5RZAUH-hpgYfmr3SxVg6-TdI",
+  authDomain: "nextfire-f878b.firebaseapp.com",
+  projectId: "nextfire-f878b",
+  storageBucket: "nextfire-f878b.appspot.com",
+  messagingSenderId: "531960495717",
+  appId: "1:531960495717:web:397df87a299bd0396e108c",
+  measurementId: "G-WKFLHQN0EJ",
+};
 
-  
 if (!firebase.apps.length) {
-    firebase.initializeApp(firebaseConfig)
+  firebase.initializeApp(firebaseConfig);
 }
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 export const storage = firebase.storage();
+export const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
--- pages/enter.js
+++ pages/enter.js
@@ -1,7 +1,45 @@
-export default function EnterPage({ }) {
+import { auth, googleAuthProvider } from "../helpers/firebase";
+
+export default function EnterPage({}) {
+  const user = null;
+  const username = null;
+
+  // 1. User is signed out (Sign In Button)
+  // 2. User is signed in, but missing username (Username Form)
+  // 3. User is signed in, has username (Sign Out Button)
   return (
     <main>
-        <h1>Sign Up</h1>
+      {user ? (
+        !username ? (
+          <UsernameForm />
+        ) : (
+          <SignOutButton />
+        )
+      ) : (
+        <SignInButton />
+      )}
     </main>
-  )
+  );
+}
+
+// Sign in with Google button
+function SignInButton() {
+  const signInWithGoogle = async () => {
+    await auth.signInWithPopup(googleAuthProvider);
+  };
+
+  return (
+    <button className="btn-google" onClick={signInWithGoogle}>
+      <img src={"/google.png"} /> Sign in with Google
+    </button>
+  );
+}
+
+// Sign out button
+function SignOutButton() {
+  return <button onClick={() => auth.signOut()}>Sign Out</button>;
+}
+
+function UsernameForm() {
+  return null;
 }
