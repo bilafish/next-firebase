c871d49d173e8542a46a30b6c94f218a509db734
Jason <bilafish@gmail.com>
2021-06-04 20:58:54 +0800

    feat: add toast component

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,15 @@
-import '../styles/globals.css'
-import NavBar from '../components/NavBar'
+import "../styles/globals.css";
+import NavBar from "../components/NavBar";
+import { Toaster } from "react-hot-toast";
+
 function MyApp({ Component, pageProps }) {
-  return (<>
-    <NavBar />
-    <Component {...pageProps} />
-  </>)
+  return (
+    <>
+      <NavBar />
+      <Component {...pageProps} />
+      <Toaster />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,13 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import Loader from '../components/Loader'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import toast from "react-hot-toast";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <h1>Hello World</h1>
-      <Loader show />
+      <button onClick={() => toast.success("hello toast!")}>Toast Me</button>
     </div>
-  )
+  );
 }
