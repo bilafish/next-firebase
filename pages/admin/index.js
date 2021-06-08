caa373513329fbc443a602d9342ffb4d459bef8a
Jason <bilafish@gmail.com>
2021-06-08 23:51:49 +0800

    feat: add auth check component

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,7 +1,11 @@
-export default function AdminPostsPage({ }) {
+import AuthCheck from "../../components/AuthCheck";
+
+export default function AdminPostsPage({}) {
   return (
     <main>
+      <AuthCheck>
         <h1>Admin</h1>
+      </AuthCheck>
     </main>
-  )
+  );
 }
